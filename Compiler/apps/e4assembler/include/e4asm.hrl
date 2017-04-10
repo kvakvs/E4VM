5b28454e49cd15597a6039fcfa296db63798a53b
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2017-04-10 12:02:46 +0200

    some opcodes

--- Compiler/apps/e4assembler/include/e4asm.hrl
+++ Compiler/apps/e4assembler/include/e4asm.hrl
@@ -1,6 +1,30 @@
 -ifndef(E4_ASSEMBLY_INC).
 -define(E4_ASSEMBLY_INC, 1).
 
+-type label() :: {f, integer()}.
+-type xreg() :: {x, integer()}.
 
+-type e4fun() :: {e4fun, #{
+  name => atom(),
+  arity => integer(),
+  code => list()
+}}.
+
+-type e4bif() :: {e4bif, #{
+  name => atom(),
+  fail => label() | ignore,
+  args => list(),
+  result => xreg() | ignore
+}}.
+
+-type e4call() :: {e4call, #{
+  target => label(),
+  arity => integer(),
+  tailcall => boolean()
+}}.
+
+-type e4ret() :: {e4ret, #{
+  dealloc => integer()
+}}.
 
 -endif.
--- Compiler/apps/e4compiler/src/e4c_pass_S.erl
+++ Compiler/apps/e4compiler/src/e4c_pass_S.erl
@@ -19,13 +19,60 @@ process({_Module, _Exports, _Attrs, Forms, _}) ->
 process_forms([], Out) -> lists:reverse(Out);
 process_forms([{function, Name, Arity, _, Code} | Forms], Out) ->
   io:format("FUNC ~s/~B~n~p~n", [Name, Arity, Code]),
-  Out1 = [process_fun(Code, []) | Out],
+  F = {e4fun, #{
+    name => Name,
+    arity => Arity,
+    code => process_fun(Code, [])
+  }},
+  Out1 = [F | Out],
   process_forms(Forms, Out1);
-process_forms([X | Forms], _Out) ->
+process_forms([X | _Forms], _Out) ->
   ?COMPILE_ERROR("Unknown form ~p", [X]).
 
 process_fun([], Out) -> lists:reverse(Out);
-process_fun([Code | Tail], Out) ->
-  process_fun(Tail, [Code | Out]);
-process_fun([X | _Code], _Out) ->
-  ?COMPILE_ERROR("Unknown instruction ~p", [X]).
+process_fun([{gc_bif, Name, Fail, _1, Args, Result} | Tail], Out) ->
+  Cmd = {e4bif, #{
+    name => Name,
+    fail => Fail,
+    args => Args,
+    result => Result
+  }},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{test, F, Fail, Args} | Tail], Out) ->
+  Cmd = {e4bif, #{
+    name => F,
+    fail => Fail,
+    args => Args,
+    result => ignore
+  }},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{func_info, _M, F, Arity} | Tail], Out) ->
+  Cmd = {e4bif, #{
+    name => function_clause,
+    fail => ignore,
+    args => [F, Arity],
+    result => ignore
+  }},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{call, Arity, Label} | Tail], Out) ->
+  Cmd = {e4call, #{
+    arity => Arity,
+    target => Label,
+    tailcall => false
+  }},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{call_only, Arity, Label} | Tail], Out) ->
+  Cmd = {e4call, #{
+    arity => Arity,
+    target => Label,
+    tailcall => true
+  }},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{deallocate, N}, return | Tail], Out) ->
+  Cmd = {e4ret, #{dealloc => N}},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([return | Tail], Out) ->
+  Cmd = {e4ret, #{dealloc => 0}},
+  process_fun(Tail, [Cmd | Out]);
+process_fun([{line, _} | Tail], Out) -> process_fun(Tail, Out);
+process_fun([Code | Tail], Out) -> process_fun(Tail, [Code | Out]).
