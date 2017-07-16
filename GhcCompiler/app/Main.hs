e5ebfddd551b33dbf76c6cae2849cc59a98b10d0
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2017-07-16 23:14:42 +0200

    some debug and logging to stderr

--- GhcCompiler/app/Main.hs
+++ GhcCompiler/app/Main.hs
@@ -3,11 +3,39 @@ module Main where
 import BeamSParser
 import System.IO
 import System.Environment
+import System.Log.Logger
+import System.Log.Handler.Syslog
+import System.Log.Handler.Simple
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter
+
+
+transpile :: String -> String -> IO ()
+transpile fileName contents =
+  let out1 = stageParseBeamS contents
+  in putStrLn "done"
+
+
+stageParseBeamS :: String -> Expr
+stageParseBeamS contents =
+  case BeamSParser.parseS contents of
+    Left e ->
+      let io2 = errorM "uerlc" $ show e
+      in ErlList [];
+    Right result ->
+      let io1 = infoM "uerlc" (show result)
+      in result
+
+
+initLogging = do
+  s <- openlog "SyslogStuff" [PID] USER DEBUG
+  updateGlobalLogger rootLoggerName (addHandler s)
+  updateGlobalLogger "uerlc" (setLevel DEBUG)
+
 
 main = do
+  initLogging
   [fileName]  <- getArgs
   fh <- openFile fileName ReadMode
   contents <- hGetContents fh
-  case BeamSParser.parseS contents of
-    Left e -> error $ show e;
-    Right result -> print result
+  transpile fileName contents
---
+++ GhcCompiler/src/Uerlc.hs
@@ -0,0 +1,3 @@
+module Uerlc where
+
+ok = ()
